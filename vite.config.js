031eb192d61381d9ec0e48256de3748bd3ae19ef
Mostafa Mohamed <mmostafamohamedelking@gmail.com>
2026-09-23 12:15:55 +0300

    mmm

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-  base: '/Makalah/',
+  plugins: [react() , tailwindcss()] ,
 })
